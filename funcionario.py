f5c10baf6876cdefb0a0175d90e06b01adc6630d
AlcidesNeto32 <Alcidesnda@gmail.com>
2026-05-23 00:04:24 -0300

    My First Commit

---
+++ funcionario.py
@@ -0,0 +1,296 @@
+from datetime import datetime as dt
+import csv,re,time,pandas as pd
+
+class Funcionario:
+    
+    def __init__(self,nome,sexo,cpf,email,telefone,data_nascimento,departamento,cargo):
+        self.__nome = nome
+        self.__sexo = sexo
+        self.__cpf = cpf
+        self.__email = email
+        self.__telefone = telefone
+        self.__data_nascimento = data_nascimento
+        self.__departamento = departamento
+        self.__cargo = cargo
+
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @nome.setter
+    def nome(self,nome):
+        self.__nome = nome
+    
+    @property
+    def sexo(self):
+        return self.__sexo
+    
+    @sexo.setter
+    def sexo(self,sexo):
+        self.__sexo = sexo
+        
+    @property
+    def cpf(self):
+        return self.__cpf
+    
+    @property
+    def email(self):
+        return self.__email
+    
+    @email.setter
+    def email(self,email): 
+        self.__email = email
+    
+    @property
+    def telefone(self):
+        return self.__telefone
+
+    @telefone.setter
+    def telefone(self,telefone):
+        self.__telefone = telefone
+    
+    @property
+    def data_nascimento(self):
+        return self.__data_nascimento
+    
+    @property
+    def departamento(self):
+        return self.__departamento
+    
+    @departamento.setter
+    def departamento(self,departamento):
+        self.__departamento = departamento
+        
+    @property
+    def cargo(self):
+        return self.__cargo
+
+    @cargo.setter
+    def cargo(self,cargo):
+        self.__cargo = cargo
+    
+    
+    def __str__(self):
+        return f"""
+NOME: {self.__nome}
+SEXO: {self.__sexo}
+CPF: {self.__cpf}
+EMAIL: {self.__email}
+TELEFONE: {self.__telefone}
+DATA NASCIMENTO: {self.__data_nascimento}
+DEPARTAMENTO: {self.__departamento}
+CARGO: {self.__cargo}
+"""
+
+class FuncionarioService:
+    
+    def cadastrar_funcionario(self,Funcionario):
+         if len(Funcionario.nome) < 10:
+             return "NOME INVÁLIDO!"
+         if not validate_cpf(cpf=Funcionario.cpf):
+             return 'CPF INVÁLIDO!'
+         if not Funcionario.telefone.isdigit() or len(Funcionario.telefone) != 9:
+             return 'TELEFONE INVÁLIDO!'
+         if Funcionario.data_nascimento.year > dt.now().year:
+             return "DATA INVÁLIDA!"
+         if dt.now().year - Funcionario.data_nascimento.year < 18:
+             return "O CANDIDATO DEVE SER MAIOR DE IDADE!"
+         if not validate_email(Funcionario.email):
+             return "EMAIL INVÁLIDO!"
+         if Funcionario.sexo != "M" and Funcionario.sexo != "F":
+             return "SEXO INVÁLIDO!"
+         if self.busca_funcionario(Funcionario.cpf) == "Funcionário não encontrado":
+             return "CPF JÁ CADASTRADO EM NOSSO SISTEMA!"
+
+         dados_func = {"nome":Funcionario.nome,
+                     "sexo":Funcionario.sexo,
+                     "cpf":Funcionario.cpf,
+                     "email":Funcionario.email,
+                     "telefone":Funcionario.telefone,
+                     "data_nascimento":Funcionario.data_nascimento,
+                     "departamento":Funcionario.departamento,
+                     "cargo":Funcionario.cargo,
+                     }
+
+         with open("/home/alcides-neto/Documents/EstudandoCobraLaele/read_files/funcionarios.csv","a",newline="") as func_file:
+             campos = ["nome","sexo","cpf","email","telefone","data_nascimento","departamento","cargo"]
+             preencher_file = csv.DictWriter(func_file,fieldnames=campos)
+             preencher_file.writerow(
+                 {"nome":dados_func['nome'],
+                 "sexo":dados_func['sexo'],
+                 "cpf":dados_func['cpf'],
+                 "email":dados_func['email'],
+                 "telefone":dados_func['telefone'],
+                 "data_nascimento":dados_func['data_nascimento'],
+                 "departamento":dados_func['departamento'],
+                 "cargo":dados_func['cargo']}
+                                     )
+         return "Funcionário(a) cadastrado com sucesso!"
+    
+    def busca_funcionario(self,cpf):
+        if not validate_cpf(cpf):
+            return("CPF INVÁLIDO!")
+                    
+        with open("/home/alcides-neto/Documents/EstudandoCobraLaele/read_files/funcionarios.csv","r") as func_file:
+            funcionarios_dict = csv.DictReader(func_file)
+            for funcionario in funcionarios_dict:
+                if funcionario['cpf'] == cpf:
+                            return f"""
+    NOME: {funcionario['nome']}
+    SEXO: {funcionario['sexo']}
+    CPF: {funcionario['cpf']}
+    DATA NASCIMENTO: {funcionario['data_nascimento']}
+    DEPARTAMENTO: {funcionario['departamento']}
+    CARGO: {funcionario['cargo']}"""
+        return "Funcionário(a) não encontrado"
+    
+    def atualizar_dados_funcionario(self,nome,sexo,cpf,email,telefone,departamento,cargo):
+        
+       with open("/home/alcides-neto/Documents/EstudandoCobraLaele/read_files/funcionarios.csv","r") as func_file:
+            funcionarios = list(csv.DictReader(func_file))
+            
+       for funcionario in funcionarios:
+           if funcionario['cpf'] == cpf:
+               funcionario.update({
+                   'nome':nome,'sexo':sexo,
+                   'email':email,'telefone':telefone,
+                   'departamento':departamento,'cargo':cargo})
+               break
+       with open("/home/alcides-neto/Documents/EstudandoCobraLaele/read_files/funcionarios.csv","w") as func_file:
+            cabecalho = ['nome','sexo','cpf','email','telefone','data_nascimento','departamento','cargo']
+            writer = csv.DictWriter(func_file,fieldnames=cabecalho)
+            writer.writeheader()
+            writer.writerows(funcionarios)
+   
+    @staticmethod
+    def listar_funcionarios():
+        with open("/home/alcides-neto/Documents/EstudandoCobraLaele/read_files/funcionarios.csv","r") as func_csv:
+            dict_funcionarios = list(csv.DictReader(func_csv))
+        
+        if len(dict_funcionarios) == 0:
+            print("Não há funcionários cadastrados.")
+            return
+        
+        for funcionario in dict_funcionarios:
+            print(f"""
+NOME: {funcionario['nome']}
+SEXO: {funcionario['sexo']}
+CPF: {funcionario['cpf']}
+EMAIL: {funcionario['email']}
+TELEFONE: {funcionario['telefone']}
+DATA NASCIMENTO: {funcionario['data_nascimento']}
+DEPARTAMENTO: {funcionario['departamento']}
+CARGO: {funcionario['cargo']}
+""")
+
+       
+menu = """
+---------------------------------------------
+MENU
+---------------------------------------------
+1.CADASTRAR FUNCIONÁRIO
+2.PESQUISAR FUNCIONÁRIO
+3.ATUALIZAR DADOS DO FUNCIONÁRIO
+4.LISTAR FUNCIONÁRIOS
+5.EMITIR RELATÓRIO GERAL
+6.SAIR
+
+SELECIONE A OPÇÃO:
+"""
+
+def relatorio_geral():
+        funcionarios = pd.read_csv("/home/alcides-neto/Documents/EstudandoCobraLaele/read_files/funcionarios.csv")
+        print(funcionarios.departamento)
+                    
+#validação simples
+def validate_email(email):
+    regex_email = "^\S+@\S+\.\S+$"
+    return re.match(regex_email,email)
+    
+    
+def validate_cpf(cpf: str) -> bool:
+
+    # Verifica a formatação do CPF
+    if not re.match(r'\d{3}\.\d{3}\.\d{3}-\d{2}', cpf):
+        return False
+
+    # Obtém apenas os números do CPF, ignorando pontuações
+    numbers = [int(digit) for digit in cpf if digit.isdigit()]
+
+    # Verifica se o CPF possui 11 números ou se todos são iguais:
+    if len(numbers) != 11 or len(set(numbers)) == 1:
+        return False
+
+    # Validação do primeiro dígito verificador:
+    sum_of_products = sum(a*b for a, b in zip(numbers[0:9], range(10, 1, -1)))
+    expected_digit = (sum_of_products * 10 % 11) % 10
+    if numbers[9] != expected_digit:
+        return False
+
+    # Validação do segundo dígito verificador:
+    sum_of_products = sum(a*b for a, b in zip(numbers[0:10], range(11, 1, -1)))
+    expected_digit = (sum_of_products * 10 % 11) % 10
+    if numbers[10] != expected_digit:
+        return False
+
+    return True
+
+
+while True:
+    opcao = int(input(menu))
+    func_service = FuncionarioService()
+    try:
+        match opcao:
+            case 1:
+                nome = input("NOME: ")
+                sexo = input("SEXO M/F: ")
+                cpf = input("CPF: ")
+                email = input("EMAIL: ")
+                telefone = input("TELEFONE: ")
+                departamento = input("DEPARTAMENTO: ")
+                cargo = input("CARGO: ")
+                try:
+                    data_nascimento = dt.strptime(input("DATA DE NASCIMENTO (DD/MM/AAAA): "),'%d/%m/%Y').date()
+                    print("Verificando informações",end="",flush=True)
+                    for i in range(3):
+                        print(".",end="",flush=True)
+                        time.sleep(0.5)
+                    print()
+                    funcionario = Funcionario(nome,sexo,cpf,email,telefone,data_nascimento,departamento,cargo)
+                    print(FuncionarioService.cadastrar_funcionario(funcionario))
+                except ValueError:
+                    print("Data inválida!")
+            case 2:
+                cpf = input("Digite o CPF do funcionário: ")
+                funcionario = func_service.busca_funcionario(cpf)
+                print(funcionario)
+            case 3:
+                nome = input("NOME: ")
+                sexo = input("SEXO: ")
+                cpf = input("CPF: ")
+                email = input("EMAIL: ")
+                telefone = input("TELEFONE: ")
+                departamento = input("DEPARTAMENTO: ")
+                cargo = input("CARGO: ")
+                print("Atualizando",end="",flush=True)
+                for i in range(3):
+                    print(".",end="",flush=True)
+                    time.sleep(0.5)
+                print()
+                print(func_service.atualizar_dados_funcionario(nome,sexo,cpf,email,telefone,departamento,cargo))
+            case 4:
+                func_service.listar_funcionarios()
+            case 5:
+                relatorio_geral()
+            case 6:
+
+                print("Saindo",end="",flush=True)
+                for i in range(3):
+                    print(".",end="",flush=True)
+                    time.sleep(0.5)
+                break
+            case _:
+                print("Essa opção não existe!")
+    except ValueError:
+        print("Digite o valor da opção correspondente!")
+
